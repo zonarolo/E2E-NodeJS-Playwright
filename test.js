b04cc3c63c4420bc2afea53ea1a5ee0ebc254ea6
Juan Rolo <juanluis211@outlook.com>
2021-05-16 13:33:37 +0200

    Wait for the page load

--- test.js
+++ test.js
@@ -18,4 +18,17 @@ const playwright = require("playwright");
   //navigate to the page
 
   await page.goto("https://web.gencat.cat/ca/inici");
+
+  // Wait for the page load
+
+  let waitPeriod = 1;
+  await page.waitForResponse((response) => {
+    console.log("Starting to wait ...." + waitPeriod);
+    waitPeriod++;
+    return response.request().resourceType() === "xhr";
+  });
+
+  await page.screenshot({ path: `./screenshots/ch-${Date.now}.png` });
+
+  await browser.close();
 })();
